3b8250f68e856962fe7c72b88417bca82155a72f
Lee Bardon <leerbardon@gmail.com>
2021-06-07 23:54:02 +0100

    converted unittest to native pytest

--- api/pestcontrol/universities/tests/test_api.py
+++ api/pestcontrol/universities/tests/test_api.py
@@ -1,31 +1,27 @@
 import json
 import pytest
 import logging
-from unittest import TestCase
-from django.test import Client
 from django.urls import reverse
 from api.pestcontrol.universities.models import University
 
 logger = logging.getLogger("UNI_LOGS")
 unis_url = reverse("universities-list")
-
-@pytest.mark.django_db
-class BasicUniversityAPITestCase(TestCase):
-    def setUp(self) -> None:
-        self.client = Client()
-        self.unis_url = reverse("universities-list")
-
-    def tearDown(self) -> None:
-        pass
+pytestmark = (
+    pytest.mark.django_db
+)  # (sets global @pytest.mark.django_db decorator for all functions in file)
 
 
-@pytest.mark.django_db
+###############################
+###  TEST GET UNIVERSITIES  ###
+###############################
+
+
 def test_zero_universities_should_return_empty_list(client) -> None:
     response = client.get(unis_url)
     assert response.status_code == 200
     assert json.loads(response.content) == []
 
-@pytest.mark.django_db
+
 def test_one_university_exists_should_succeed(client) -> None:
     test_uni = University.objects.create(name="Southampton")
     response = client.get(unis_url)
@@ -33,27 +29,31 @@ def test_one_university_exists_should_succeed(client) -> None:
     assert response.status_code == 200
     assert response_content.get("name") == test_uni.name
     assert response_content.get("status") == test_uni.status
-    assert (
-        response_content.get("application_link") == test_uni.application_link
-    )
+    assert response_content.get("application_link") == test_uni.application_link
     assert response_content.get("notes") == test_uni.notes
     test_uni.delete()
 
 
-@pytest.mark.django_db
+###############################
+###  TEST POST UNIVERSITIES ###
+###############################
+
+
 def test_create_university_without_arguments_should_fail(client) -> None:
     res = client.post(path=unis_url)
-    assert  res.status_code == 400
-    assert  json.loads(res.content) == {"name": ["This field is required."]}
-    
-@pytest.mark.django_db
+    assert res.status_code == 400
+    assert json.loads(res.content) == {"name": ["This field is required."]}
+
+
 def test_create_existing_university_should_fail(client) -> None:
     University.objects.create(name="Southampton")
     res = client.post(path=unis_url, data={"name": "Southampton"})
     assert res.status_code == 400
-    assert json.loads(res.content) == {"name": ["university with this name already exists."]}
-    
-@pytest.mark.django_db
+    assert json.loads(res.content) == {
+        "name": ["university with this name already exists."]
+    }
+
+
 def test_create_university_with_name_and_default_fields_only(client) -> None:
     res = client.post(path=unis_url, data={"name": "test uni"})
     response_content = json.loads(res.content)
@@ -63,16 +63,14 @@ def test_create_university_with_name_and_default_fields_only(client) -> None:
     assert response_content.get("application_link") == ""
     assert response_content.get("notes") == ""
 
-@pytest.mark.django_db
+
 def test_create_uni_with_layoffs_status_should_succeed(client) -> None:
-    res = client.post(
-        path=unis_url, data={"name": "test uni", "status": "Layoffs"}
-    )
+    res = client.post(path=unis_url, data={"name": "test uni", "status": "Layoffs"})
     assert res.status_code == 201
     res_content = json.loads(res.content)
-    assert  res_content.get("status") == "Layoffs"
+    assert res_content.get("status") == "Layoffs"
 
-@pytest.mark.django_db
+
 def test_create_uni_with_wrong_status_should_fail(client) -> None:
     res = client.post(
         path=unis_url, data={"name": "test uni", "status": "VeryWrongIndeed"}
@@ -82,13 +80,15 @@ def test_create_uni_with_wrong_status_should_fail(client) -> None:
     assert "VeryWrongIndeed" in str(res.content)
     assert "is not a valid choice" in str(res.content)
 
+
 @pytest.mark.xfail
 def test_should_b_ok_if_fails() -> None:
-    assert (1, 2)
+    assert 1 == 2
+
 
 @pytest.mark.skip
 def test_should_be_skipped() -> None:
-    assert (1, 2)
+    assert 1 == 2
 
 
 def raise_big_ole_exception() -> None:
