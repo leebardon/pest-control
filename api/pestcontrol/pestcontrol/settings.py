3d111d989c4d4139390aaf58d7485db6ef226937
Lee Bardon <leerbardon@gmail.com>
2021-06-09 20:22:05 +0100

    email set up

--- api/pestcontrol/pestcontrol/settings.py
+++ api/pestcontrol/pestcontrol/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = "sillyemail2000@gmail.com"
+EMAIL_HOST_USER = "smtp.gmail.com"
+EMAIL_HOST_PASSWORD = "SillyPassword"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
